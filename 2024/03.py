fbb082322d20cc203b139f52af65ed06349e5128
Ty Kennedy <ty.j.kennedy@gmail.com>
2024-12-05 06:32:03 -0500

    Added part 2 functionality

--- 2024/03.py
+++ 2024/03.py
@@ -17,6 +17,7 @@ command_mul = re.compile("mul\(\d+,\d+\)")
 command_do = re.compile("do\(\)")
 command_dont = re.compile("don't\(\)")
 
+
 def part_1(input_: Iterable[str]) -> int:
     input_ = _add_new_lines(input_)
     items = map(_get_nums, command_mul.findall(input_))
@@ -29,16 +30,28 @@ def part_1(input_: Iterable[str]) -> int:
 
 
 def part_2(input_: Iterable[str]):
+    DO = 1
+    DONT = 2
     input_ = _add_new_lines(input_)
-    input_minus_dont = re.compile("don't\(\).*do\(\)").sub("", input_)
-    input_minus_dont = re.compile("don't\(\)").split(input_minus_dont)[0]
-    items = map(_get_nums, command_mul.findall(input_minus_dont))
+    items = re.compile("do\(\)|don't\(\)|mul\(\d+,\d+\)").findall(input_)
 
+    do_flag = DO
     sum_ = 0
     for item in items:
-        sum_ += math.prod(item)
+        if do_flag == DO and item[0] == 'm':
+            sum_ += math.prod(_get_nums(item))
+            continue
+
+        if do_flag == DO and item == "don't()":
+            do_flag = DONT
+            continue
+
+        if do_flag == DONT and item == "do()":
+            do_flag = DO
+            continue
 
     return sum_
+
 
 def _add_new_lines(lines: Iterable[str]) -> str:
     """Add new-line escape sequences to a list of strings.
@@ -53,6 +66,7 @@ def _add_new_lines(lines: Iterable[str]) -> str:
 def _get_nums(s: str) -> tuple[int, int]:
     i1, i2 = s.removeprefix('mul(').removesuffix(')').split(',')
     return int(i1), int(i2)
+
 
 if __name__ == '__main__':
     aoc_main('', [part_1, part_2])  # TODO Add file name
